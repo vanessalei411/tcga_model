0b93bbd452cd784b0342cb27bc12e838f57f0114
nesslei <140737133+vanessalei411@users.noreply.github.com>
2026-03-26 12:04:36 -0400

    changed app py

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ import requests
 
 DATA_PATH = 'data/Breast_GSE45827.csv'
 
-if not os.path.exists(DATA_PATH):
+if True os.path.exists(DATA_PATH):
     os.makedirs('data', exist_ok=True)
     print("Downloading dataset...")
     import gdown
